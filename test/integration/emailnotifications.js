20a8623117f90554796af175f0837bcf803ea5aa
Matias Alejo Garcia <ematiu@gmail.com>
2018-09-05 16:44:27 -0300

    refactor getTxHistory

--- test/integration/emailnotifications.js
+++ test/integration/emailnotifications.js
@@ -86,6 +86,7 @@ describe('Email notifications', function() {
           _readTemplateFile_old.call(emailService, language, filename, cb);
         }
       };
+
       helpers.stubUtxos(server, wallet, [1, 1], function() {
         var txOpts = {
           outputs: [{
